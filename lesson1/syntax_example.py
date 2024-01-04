e07db782cb22ad2b392347afad72379f171fa4a8
Buz88 <120252021+Buz88@users.noreply.github.com>
2024-01-04 17:00:53 +0100

    test

---
+++ lesson1/syntax_example.py
@@ -0,0 +1,43 @@
+#сортування імпортів ознайомитися в PEP 8
+
+
+import requests
+import random
+
+
+'''
+багатострічний коментар
+замість #
+'''
+
+
+a = "hello"     #a is a string
+b = "hello"
+c = "hello"
+f = "hello"
+
+
+#памятати про стиль лапок - одиночний коментар
+
+
+value_1 = 1
+value_contains_one = 1
+value_contains_two = 2
+value_contains_three = 3
+
+
+#ogoloshennia_funkcii
+
+
+def my_func():
+    print('hello my friend')
+
+
+#velyki_litery dlya ogoloshennia klasiv
+#zminni malinki z nijnim pidkreslenniam a v klassah camelcase
+
+
+class MyClass:
+    pass
+
+
