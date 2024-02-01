3991c8fbead6fada3462c3a5b284a66b63966523
Buz88 <120252021+Buz88@users.noreply.github.com>
2024-02-01 15:29:04 +0100

    homework7

--- homework_lesson7/homework7.py
+++ homework_lesson7/homework7.py
@@ -16,9 +16,9 @@ b = [22,3,44,5,8888,9345,'sad']
 
 intersection = list(find_intersection(a, b))
 print(intersection)
-
+'''
 # Zadacha 2
-number = lambda s: s.isdigit()  if s.isdigit() else False
+number = lambda s: s.isnumeric() if s.isnumeric() else (s.replace('.', '', 1).isdigit() if '.' in s else False)
 
 input_1 = input('Enter a number: ')
 result = number(input_1)
@@ -27,8 +27,8 @@ if result == True:
     print(f'{input_1} is a number')
 else:
     print(f'{input_1} is not a number')
-'''
 
+'''
 # Zadacha 3
 lists = [[1,2,3,4,5,6,7,8,9,10],[1,2,3,4,5,6,7,8,9],[1,2,3],[1,2],[2]]
 find_min = lambda list: (min(list, key=lambda x: len(x)))
@@ -36,6 +36,6 @@ find_max = lambda list: (max(list, key=lambda x: len(x)))
 
 print(f'Мінімальне значення {find_min(lists)} та максимальне значення твого листу: {find_max(lists)}')
 print(f'Мінімальне значення {len(find_min(lists))} та максимальне значення твого листу: {len(find_max(lists))}')
-
+'''
 
 
